57888ec99a6b44832474356d87f89aad0a1c291f
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-09-28 18:09:53 -0300

    actualizacion front login
    fecha: 28-09-2024

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -28,28 +28,19 @@
         <div class="mb-5">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
             <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nombre@dominio.com" value="{{ old('email') }}" required />
-            <!-- Error de correo -->
-            @error('email')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ $message }}</p>
-            @enderror
+        
         </div>
         <!-- Campo de contraseña -->
         <div class="mb-5">
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
             <input type="password" placeholder="••••••••" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-none focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-            <!-- Error de contraseña -->
-            @error('password')
-                <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ $message }}</p>
-            @enderror
+          
         </div>
         <!-- Botón de enviar -->
         <div class="flex justify-center mb-5">
             <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-none text-sm px-5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-900">Acceder</button>
         </div>
-        <!-- Mensaje de sesión -->
-        @if(session('error'))
-            <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ session('error') }}</p>
-        @endif
+    
     </form>
 </body>
 </html>
