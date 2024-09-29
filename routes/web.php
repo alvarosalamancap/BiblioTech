a72ceb4f11d3bc8a22bf6f2faedf634a24fff123
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-09-29 19:53:40 -0300

    actualizo web.php
    agrego el loginController al web.php
    29-09-2024

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
-
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+
+// Ruta para mostrar el formulario de login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('loginForm');
+
+// Ruta para procesar el login
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/', function () {
     return view('welcome');
