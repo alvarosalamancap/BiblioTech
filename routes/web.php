ba856e29f144e5f30a5aa5357ec61412e70b261b
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-10-08 19:45:16 -0300

    actualizacion

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('loginForm
 // Ruta para procesar el login
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
