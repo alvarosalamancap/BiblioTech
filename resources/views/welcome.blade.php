6537eec2a05710d4c6a76d8b65dad08d73a9d44c
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-10-10 00:08:14 -0300

    creo pagina de inicio
    10-10-2024 se crea la pagina de inicio en welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Inicio - Bibliotech</title>
+    <title>Pagina de Inicio - Bibliotech</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#618985] flex items-center justify-center h-screen">
