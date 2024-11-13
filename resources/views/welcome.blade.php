1ed8f00718feca86727f64d2ec605361fc037f22
alvarosalamancax <alvaro.salamancape@gmail.com>
2024-11-12 23:51:34 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="bg-[#618985] flex items-center justify-center h-screen relative">
     <!-- Botón de Cambiar Contraseña -->
-   
+    <a href="{{ route('password.change') }}" class="absolute top-4 right-4 bg-[#96bbbb] text-white px-6 py-2 rounded-full shadow-lg hover:bg-[#7ea6a6]">
         Cambiar Contraseña
     </a>
 
